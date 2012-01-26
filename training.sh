cb16b5c8971fe446459822dc3624172e0f4e96c2
Robby Cochran <Robby Cochran>
2012-01-26 16:40:37 -0500

    initial LSF support for killdevil

--- training.sh
+++ training.sh
@@ -14,6 +14,7 @@ HERE="`dirname "$WRAPPER"`"
 # Command line options
 VERBOSE_OUTPUT=0
 MAKE_THREADS=4
+USE_LSF=0
 ROOT_DIR="`pwd`"
 	
 # Cliver options
@@ -38,13 +39,14 @@ initialize_training()
 {
 	KTEST_DIR="$DATA_DIR/network/tetrinet/last-run"
 	TRAINING_DIR=$DATA_DIR/$CLIVER_MODE/$(basename $BC_FILE .bc)
+
 	CLIVER_OUTPUT_DIR=$TRAINING_DIR/$RUN_PREFIX
 	
 	CLIVER_BIN="$KLEE_ROOT/bin/cliver"
 
 	BC_FILE="$TETRINET_ROOT/bin/tetrinet-klee.bc"
 
-	leval mkdir -p $TRAINING_DIR
+	leval mkdir -p $CLIVER_OUTPUT_DIR
 	leval ln -sf $CLIVER_OUTPUT_DIR $TRAINING_DIR/recent
 }
 
@@ -52,18 +54,20 @@ tetrinet_training()
 {
 	for i in $KTEST_DIR/*ktest; do
 
+		local ktest_basename=$(basename $i .ktest)
 		local cliver_opts=$CLIVER_OPTS
 		cliver_opts+=" -socket-log $i "
+		cliver_opts+=" -output-dir $CLIVER_OUTPUT_DIR/$ktest_basename "
 
-	  random_seed=$(parse_ktest_file $i 2)
-		starting_height=$(parse_ktest_file $i 2)
-		input_gen_type=$(parse_ktest_file $i 3)
-		partial_type=$(parse_ktest_file $i 4)
-		partial_rate=$(parse_ktest_file $i 5)
-		player_name="$(parse_ktest_file $i 7)"
-		server_address="$(parse_ktest_file $i 8)"
+	  local random_seed=$(parse_ktest_file $i 2)
+		local starting_height=$(parse_ktest_file $i 2)
+		local input_gen_type=$(parse_ktest_file $i 3)
+		local partial_type=$(parse_ktest_file $i 4)
+		local partial_rate=$(parse_ktest_file $i 5)
+		local player_name="$(parse_ktest_file $i 7)"
+		local server_address="$(parse_ktest_file $i 8)"
 
-		bc_file_opts="-autostart "
+		local bc_file_opts="-autostart "
 		bc_file_opts+="-startingheight $starting_height "
 		bc_file_opts+="-partialtype $partial_type "
 		bc_file_opts+="-partialrate $partial_rate "
@@ -72,7 +76,11 @@ tetrinet_training()
 
 		cliver_opts+="$BC_FILE $bc_file_opts "
 
-		leval $CLIVER_BIN $cliver_opts
+		if [ $USE_LSF -eq 1]; then
+			lbsub $CLIVER_BIN $cliver_opts
+		else
+			leval $CLIVER_BIN $cliver_opts
+		fi
 
 	done
 }
@@ -83,7 +91,6 @@ do_training()
 
 	CLIVER_OPTS+="-libc=uclibc -posix-runtime "
 	CLIVER_OPTS+="-pc-single-line -debug-stderr -emit-all-errors "
-	CLIVER_OPTS+="-output-dir $CLIVER_OUTPUT_DIR/"
 	CLIVER_OPTS+="-switch-type=$SWITCH_TYPE "
 	CLIVER_OPTS+="-output-source=$OUTPUT_LLVM_ASSEMBLY "
 	CLIVER_OPTS+="-max-memory=$MAX_MEMORY "
@@ -95,6 +102,7 @@ do_training()
 	CLIVER_OPTS+="-debug-searcher=$DEBUG_SEARCHER "
 	CLIVER_OPTS+="-debug-print-instructions=$PRINT_INSTRUCTIONS "
 	CLIVER_OPTS+="-cliver-mode=$CLIVER_MODE "
+	#CLIVER_OPTS+="-output-dir $CLIVER_OUTPUT_DIR/"
 
 	tetrinet_training
 
@@ -103,8 +111,12 @@ do_training()
 
 main() 
 {
-  while getopts ":vr:j:" opt; do
+  while getopts ":vr:j:bl" opt; do
     case $opt in
+      l)
+        USE_LSF=1
+				;;
+
       v)
         VERBOSE_OUTPUT=1
         ;;
