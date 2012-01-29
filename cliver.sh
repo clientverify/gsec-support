d73fdf2f4b8fadcdc3b0bc1052073b0eb54e7ace
Robby Cochran <Robby Cochran>
2012-01-28 19:16:00 -0500

    cliver.sh: added gdb and debugging commandline options, cleaned up code

--- cliver.sh
+++ cliver.sh
@@ -15,14 +15,19 @@ HERE="`dirname "$WRAPPER"`"
 VERBOSE_OUTPUT=0
 MAKE_THREADS=4
 USE_LSF=0
+USE_GDB=0
 ROOT_DIR="`pwd`"
 	
-# Cliver options
+# Default cliver options
 CLIVER_MODE="training"
+CLIVER_LIBC="uclibc"
 OUTPUT_LLVM_ASSEMBLY=0
+OUTPUT_LLVM_BITCODE=0
 PRINT_INSTRUCTIONS=0
-MAX_MEMORY=64000
+MAX_MEMORY=32000
 SWITCH_TYPE="simple"
+USE_TEE_BUF=1
+DISABLE_OUTPUT=0
 DEBUG_ADDRESS_SPACE_GRAPH=0
 DEBUG_STATE_MERGER=0
 DEBUG_NETWORK_MANAGER=0
@@ -35,86 +40,127 @@ parse_ktest_file()
 	eval "basename $1 .ktest | awk -F_ '{ printf \$$2 }'"
 }
 
-initialize_training()
+parse_tetrinet_parameters()
+{
+  local random_seed=$(parse_ktest_file $1 2)
+	local starting_height=$(parse_ktest_file $1 2)
+	local input_gen_type=$(parse_ktest_file $1 3)
+	local partial_type=$(parse_ktest_file $1 4)
+	local partial_rate=$(parse_ktest_file $1 5)
+	local player_name="$(parse_ktest_file $1 7)"
+	local server_address="$(parse_ktest_file $1 8)"
+
+	local bc_file_opts="-autostart "
+	bc_file_opts+="-startingheight $starting_height "
+	bc_file_opts+="-partialtype $partial_type "
+	bc_file_opts+="-partialrate $partial_rate "
+	bc_file_opts+="-seed $random_seed "
+	bc_file_opts+=" $player_name $server_address "
+	printf "%s" "$bc_file_opts"
+}
+
+initialize_cliver()
+{
+	CLIVER_BIN="$KLEE_ROOT/bin/cliver"
+
+	BASE_OUTPUT_DIR=$DATA_DIR/$CLIVER_MODE/$(basename $BC_FILE .bc)
+
+	CLIVER_OUTPUT_DIR=$BASE_OUTPUT_DIR/$RUN_PREFIX
+
+	leval mkdir -p $CLIVER_OUTPUT_DIR
+	leval ln -sfT $RUN_PREFIX $BASE_OUTPUT_DIR/recent
+}
+
+initialize_tetrinet()
 {
 	KTEST_DIR="$DATA_DIR/network/tetrinet/last-run"
 	BC_FILE="$TETRINET_ROOT/bin/tetrinet-klee.bc"
-
-	TRAINING_DIR=$DATA_DIR/$CLIVER_MODE/$(basename $BC_FILE .bc)
-
-	CLIVER_OUTPUT_DIR=$TRAINING_DIR/$RUN_PREFIX
-	
-	CLIVER_BIN="$KLEE_ROOT/bin/cliver"
-
-	leval mkdir -p $CLIVER_OUTPUT_DIR
-	leval ln -sf $RUN_PREFIX $TRAINING_DIR/recent
 }
 
-tetrinet_training()
+cliver_parameters()
 {
-	for i in $KTEST_DIR/*ktest; do
+	local cliver_params="-posix-runtime -pc-single-line -emit-all-errors -debug-stderr "
+	cliver_params+="-no-output=$DISABLE_OUTPUT "
+	cliver_params+="-use-tee-buf=$USE_TEE_BUF "
+	cliver_params+="-libc=$CLIVER_LIBC "
+	cliver_params+="-switch-type=$SWITCH_TYPE "
+	cliver_params+="-output-source=$OUTPUT_LLVM_ASSEMBLY "
+	cliver_params+="-output-module=$OUTPUT_LLVM_BITCODE "
+	cliver_params+="-max-memory=$MAX_MEMORY "
+	cliver_params+="-always-print-object-bytes=$PRINT_OBJECT_BYTES " 
+	cliver_params+="-debug-address-space-graph=$DEBUG_ADDRESS_SPACE_GRAPH " 
+	cliver_params+="-debug-state-merger=$DEBUG_STATE_MERGER "
+	cliver_params+="-debug-network-manager=$DEBUG_NETWORK_MANAGER "
+	cliver_params+="-debug-socket=$DEBUG_SOCKET "
+	cliver_params+="-debug-searcher=$DEBUG_SEARCHER "
+	cliver_params+="-debug-print-instructions=$PRINT_INSTRUCTIONS "
+	cliver_params+="-cliver-mode=$CLIVER_MODE "
+	printf "%s" "$cliver_params"
+}
 
-		local ktest_basename=$(basename $i .ktest)
-		local cliver_opts=$CLIVER_OPTS
-		cliver_opts+=" -socket-log $i "
-		cliver_opts+=" -output-dir $CLIVER_OUTPUT_DIR/$ktest_basename "
-
-	  local random_seed=$(parse_ktest_file $i 2)
-		local starting_height=$(parse_ktest_file $i 2)
-		local input_gen_type=$(parse_ktest_file $i 3)
-		local partial_type=$(parse_ktest_file $i 4)
-		local partial_rate=$(parse_ktest_file $i 5)
-		local player_name="$(parse_ktest_file $i 7)"
-		local server_address="$(parse_ktest_file $i 8)"
-
-		local bc_file_opts="-autostart "
-		bc_file_opts+="-startingheight $starting_height "
-		bc_file_opts+="-partialtype $partial_type "
-		bc_file_opts+="-partialrate $partial_rate "
-		bc_file_opts+="-seed $random_seed "
-		bc_file_opts+=" $player_name $server_address "
-
-		cliver_opts+="$BC_FILE $bc_file_opts "
-
-		if [ $USE_LSF -eq 1 ]; then
-			lbsub $CLIVER_BIN $cliver_opts
-		else
-			leval $CLIVER_BIN $cliver_opts
-		fi
-
-	done
+run_cliver()
+{
+ 	if [ $USE_LSF -eq 1 ]; then
+ 		lbsub $CLIVER_BIN $@
+ 	elif [ $USE_GDB -eq 1 ]; then
+ 		geval $CLIVER_BIN-bin $@
+ 	else
+ 		leval $CLIVER_BIN-bin $@
+ 	fi
 }
 
 do_training()
 {
-  echo "[cliver training]"
+	for i in $KTEST_DIR/*ktest; do
 
-	CLIVER_OPTS+="-libc=uclibc -posix-runtime "
-	CLIVER_OPTS+="-pc-single-line -debug-stderr -emit-all-errors "
-	CLIVER_OPTS+="-switch-type=$SWITCH_TYPE "
-	CLIVER_OPTS+="-output-source=$OUTPUT_LLVM_ASSEMBLY "
-	CLIVER_OPTS+="-max-memory=$MAX_MEMORY "
-	CLIVER_OPTS+="-always-print-object-bytes=$PRINT_OBJECT_BYTES " 
-	CLIVER_OPTS+="-debug-address-space-graph=$DEBUG_ADDRESS_SPACE_GRAPH " 
-	CLIVER_OPTS+="-debug-state-merger=$DEBUG_STATE_MERGER "
-	CLIVER_OPTS+="-debug-network-manager=$DEBUG_NETWORK_MANAGER "
-	CLIVER_OPTS+="-debug-socket=$DEBUG_SOCKET "
-	CLIVER_OPTS+="-debug-searcher=$DEBUG_SEARCHER "
-	CLIVER_OPTS+="-debug-print-instructions=$PRINT_INSTRUCTIONS "
-	CLIVER_OPTS+="-cliver-mode=$CLIVER_MODE "
-	#CLIVER_OPTS+="-output-dir $CLIVER_OUTPUT_DIR/"
+		local ktest_basename=$(basename $i .ktest)
+		local cliver_params="$(cliver_parameters)"
 
-	tetrinet_training
+		cliver_params+=" -socket-log $i "
+		cliver_params+=" -output-dir $CLIVER_OUTPUT_DIR/$ktest_basename "
 
-  echo "[done]"
+		cliver_params+="$BC_FILE $(parse_tetrinet_parameters $i) "
+
+		run_cliver $cliver_params
+
+	done
+}
+
+do_verification()
+{
+	for i in $KTEST_DIR/*ktest; do
+
+		local ktest_basename=$(basename $i .ktest)
+		local cliver_params="$(cliver_parameters)"
+
+		cliver_params+=" -socket-log $i "
+		cliver_params+=" -output-dir $CLIVER_OUTPUT_DIR/$ktest_basename "
+
+		cliver_params+="$BC_FILE $(parse_tetrinet_parameters $i) "
+
+		run_cliver $cliver_params
+
+	done
 }
 
 main() 
 {
-  while getopts ":vr:j:bl" opt; do
+  while getopts ":vr:j:blm:dg" opt; do
     case $opt in
       l)
         USE_LSF=1
+				;;
+      
+			g)
+        USE_GDB=1
+				;;
+
+			d)
+				DEBUG_ADDRESS_SPACE_GRAPH=1
+				DEBUG_STATE_MERGER=1
+				DEBUG_NETWORK_MANAGER=1
+				DEBUG_SOCKET=1
+				DEBUG_SEARCHER=1
 				;;
 
       v)
@@ -125,7 +171,11 @@ main()
         echo "Setting root dir to $OPTARG"
         ROOT_DIR="$OPTARG"
         ;;
-  
+   
+      m)
+        CLIVER_MODE="$OPTARG"
+        ;;
+
       j)
         MAKE_THREADS=$OPTARG
         ;;
@@ -138,9 +188,15 @@ main()
     esac
   done
 
-  initialize_root_directories
+	echo "[cliver mode: $CLIVER_MODE]"
 
+  initialize_root_directories
   initialize_logging $@
+
+	# bc file initialization
+	initialize_tetrinet
+
+	initialize_cliver
 
 	if [ $USE_LSF -eq 1 ]; then
 		initialize_lsf
@@ -149,10 +205,19 @@ main()
   # record start time
   start_time=$(elapsed_time)
 
-	initialize_training
-	do_training
+	case $CLIVER_MODE in
 
-  echo "Elapsed time: $(elapsed_time $start_time)"
+		training )
+			do_training
+			;;
+
+		verif*)
+			do_verification
+			;;
+
+	esac
+
+  echo "[elapsed time: $(elapsed_time $start_time)]"
 }
 
 # Run main
