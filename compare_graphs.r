0766d469b063a2ccbdeca2f4a1100713a9417246
Robby Cochran <Robby Cochran>
2012-04-27 10:59:15 -0400

    compare_graphs.r: updates

--- compare_graphs.r
+++ compare_graphs.r
@@ -2,9 +2,12 @@
 
 args=paste(commandArgs(), " ")
 
+# cbgColourPalette <- scale_colour_manual(values=c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"))
+cbgColourPalette <- scale_colour_manual(values=c("#0072B2", "#56B4E9", "#E69F00", "#009E73", "#F0E442", "#999999", "#D55E00", "#CC79A7"))
+
 colnames = c(
 "STATS","round","activestates","erases","prunes",
-"time","time_user","prune_time","mergetime",
+"time","timeuser","prunetime","mergetime",
 "searchertime","solvertime","forktime", "roundinsts",
 "totalstates", "mem"
 )
@@ -12,8 +15,8 @@ colnames = c(
 plotnames = c(
 "time","solvertime", "roundinsts", "mem"
 )
-plotwidth=11
-plotheight=7
+plotwidth=9
+plotheight=6
 
 root_dir="/home/rac/research/gsec/results"
 data_dir="data"
@@ -32,29 +35,37 @@ for (i in seq(length(data_subdirs))) {
     if (class(tmp_data) != "try-error") {
       len = length(tmp_data[,1]) # length of rows, not cols
       min_size = min(min_size, len)
-      tmp_data$name=rep(file, len)
+      tmp_data$name=rep(substr(file,6, 7), len)
       tmp_data$mode=rep(data_subdirs[i], len)
       data = rbind(data, tmp_data)
     }
   }
 }
 
+min_size = min_size - 1
+
 library(ggplot2)
 
-for (p in plotnames) {
+for (pname in plotnames) {
   x_axis = "round"
-  name = paste(paste("stat",p,sep="-"), "geom-line", sep="_")
-  theplot <- ggplot(data, aes_string(x=x_axis, y=p))
-  theplot + geom_line(aes(colour=factor(mode),linetype=factor(mode))) + facet_grid(name ~ .) + opts(title=name)
+  name = paste(paste("stat",pname,sep="-"), "geom-line", sep="_")
+  p = ggplot(subset(data, round < min_size), aes_string(x=x_axis, y=pname))
+  p = p + geom_line(aes(colour=factor(mode),linetype=factor(mode)),size=1)
+  p = p + scale_fill_hue("Algorithm")
+  p = p + facet_grid(name ~ .)
+  p = p + theme_bw()
+  p = p + cbgColourPalette
+  p = p + opts(title=name)
+  p;  
   filename = paste(name, output_filetype, sep=".")
   ggsave(paste(root_dir, output_dir, filename, sep="/"), width=plotwidth, height=plotheight)
 }
 
 for (p in plotnames) {
   x_axis = "round"
-  name = paste(paste("stat",p,sep="-"), "geom-line", "yscale-log10", sep="_")
-  theplot <- ggplot(data, aes_string(x=x_axis, y=p)) 
-  theplot + geom_line(aes(colour=factor(mode),linetype=factor(mode))) + scale_y_log10() + facet_grid(name ~ .) + opts(title=name)
+  name = paste(paste("stat",p,sep="-"), "geom-line", "yscale-log2", sep="_")
+  theplot <- ggplot(subset(data, round < min_size), aes_string(x=x_axis, y=p)) 
+  theplot + geom_line(aes(colour=factor(mode),linetype=factor(mode)),size=1) + scale_y_log2() + theme_bw() + facet_grid(name ~ .) + opts(title=name)
   filename = paste(name, output_filetype, sep=".")
   ggsave(paste(root_dir, output_dir, filename, sep="/"), width=plotwidth, height=plotheight)
 }
@@ -62,7 +73,7 @@ for (p in plotnames) {
 for (p in plotnames) {
   name = paste(paste("stat",p,sep="-"),paste("round","lt",min_size,sep="-"),"func-sum","geom-bar",sep="_")
   theplot <- ggplot(subset(data, round < min_size), aes_string(x="mode", y=p)) 
-  theplot + stat_summary(fun.y="sum", geom="bar", fill="white", colour="gray") + opts(title=name)
+  theplot + stat_summary(fun.y="sum", geom="bar", fill="white", colour="gray") + theme_bw() + opts(title=name)
   filename = paste(name, output_filetype, sep=".")
   ggsave(paste(root_dir, output_dir, filename, sep="/"), width=plotwidth, height=plotheight)
 }
