5072f3fd267ec6fe227a21c43d401aaee2b37f68
Robby Cochran <rcochran@killdevil-login1.kd.unc.edu>
2012-05-18 15:50:15 -0400

    copy_results.sh: WIP

--- copy_results.sh
+++ copy_results.sh
@@ -11,6 +11,8 @@ HERE="`dirname "$WRAPPER"`"
 # Include gsec_common
 . $HERE/gsec_common
 
+VERBOSE_OUTPUT=1
+ROOT_DIR="`pwd`"
 
 usage()
 {
@@ -42,9 +44,33 @@ main()
     esac
   done
 
-  DATA_FILE="debug.txt"
-  for i in $(find $RESULTS_SOURCE -name $DATA_FILE); do
-    echo $i
+  #initialize_root_directories
+  #initialize_logging $@
+  #DRY_RUN=1
+
+  #BASE_NAME="debug.txt"
+  #for file in $( find $RESULTS_SOURCE -name $BASE_NAME ); do
+
+  PATTERN="debug.txt"
+  for dir in $RESULTS_SOURCE/* ; do
+    for file in $( find -L $dir/recent -name $PATTERN ); do
+      local stats_file="./.$(basename $0)-$RANDOM.txt"
+      #echo $file
+      #echo $stats_file
+      
+      #local dest=$RESULTS_DESTINATION/$(basename $dir)/$(basename $RESULTS_SOURCE)/$(basename $(dirname $file) ).txt
+      local dest=$RESULTS_DESTINATION/$(basename $dir)/$(basename $RESULTS_SOURCE)/
+
+      grep STATS $file > $stats_file
+      if [[ $(wc -l $stats_file | awk '{print $1}' ) -gt 0 ]]; then
+        echo "rsync -ave ssh $stats_file $dest"
+        rsync -ave ssh $stats_file $dest
+      else
+        echo "error: $file is empty"
+        rm $stats_file
+      fi
+      rm $stats_file
+    done
   done
 
 }
