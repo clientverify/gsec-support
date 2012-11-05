1864fe5ecee819d781bc1262ce1484ed39d42ba8
Robby Cochran <rac@albus.cs.unc.edu>
2012-11-04 23:23:45 -0500

    do-tests.sh

--- do-tests.sh
+++ do-tests.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-LSF_MEMORY_GB=32
-CLIENT_TYPES="xpilot tetrinet"
+LSF_MEMORY_GB=48
+#CLIENT_TYPES="xpilot tetrinet"
 #CLIENT_TYPES="xpilot"
-#CLIENT_TYPES="tetrinet"
+CLIENT_TYPES="tetrinet"
 EDIT_DIST_TYPES="edit-dist-row edit-dist-kprefix-row edit-dist-kprefix-hash"
 #KPREFIX_TYPES="edit-dist-kprefix-row edit-dist-kprefix-hash"
 KPREFIX_TYPES="edit-dist-kprefix-row"
 #KPREFIX_TYPES="edit-dist-kprefix-hash"
-#KPREFIX_LENGTHS="256 16384"
-#KPREFIX_LENGTHS="256 512 1024"
-#KPREFIX_LENGTHS="256 "
-#KPREFIX_LENGTHS="512 "
-#KPREFIX_LENGTHS="64 512 "
-#KPREFIX_LENGTHS="48 64 "
-KPREFIX_LENGTHS="16384"
+#KPREFIX_LENGTHS="256 512 1024 16384"
 #KPREFIX_LENGTHS="2048"
+KPREFIX_LENGTHS="64"
 #CLUSTER_SIZES="8 16 32 64 256 512 1024"
-#CLUSTER_SIZES="0008 0016 032 0256 0512 1024"
 CLUSTER_SIZES="64 256 4096"
-#CLUSTER_SIZES="8096"
 
 DATA_TAG="large"
 
@@ -36,29 +29,23 @@ sleep_until_jobs_finish()
 
 #=======================================================================
 
-#for client in $CLIENT_TYPES; do
-#  echo "Training -- Client: $client"
-#  ./gsec-support/cliver.sh -t training -c $client -i lsf -m $LSF_MEMORY_GB -b $DATA_TAG -d 1
-#done
-#
-#sleep_until_jobs_finish
-#exit
+for client in $CLIENT_TYPES; do
+  echo "Training -- Client: $client"
+  ./gsec-support/cliver.sh -t training -c $client -i lsf -m $LSF_MEMORY_GB -b $DATA_TAG -d 1
+done
+
+# Wait until training is finished before we start running jobs
+sleep_until_jobs_finish
 
 for client in $CLIENT_TYPES; do
   echo "Verification: -- Client: $client"
 
-  #./gsec-support/cliver.sh -t training -c $client -i lsf -m $LSF_MEMORY_GB -b $DATA_TAG
-
-  #./gsec-support/cliver.sh -t ncross-edit-dist-row -o nc-ed-row -c $client -i lsf -m $LSF_MEMORY_GB
-
   # DEBUG TESTING
-
-
   for edit_dist in $KPREFIX_TYPES; do
-
-    ./gsec-support/cliver.sh  -t "self-$edit_dist" -o "self" -c $client -i lsf -m $LSF_MEMORY_GB -x " -max-k-extension=16384 -use-clustering-hint -cluster-size=2147483648 " -b $DATA_TAG
-
     for k_length in $KPREFIX_LENGTHS; do
+
+      ./gsec-support/cliver.sh  -t "self-$edit_dist" -o "self" -c $client -i lsf -m $LSF_MEMORY_GB -x " -max-k-extension=$k_length -use-clustering-hint -cluster-size=2147483648 " -b $DATA_TAG
+
       for cluster_size in $CLUSTER_SIZES; do
         echo "K = $k_length, edit dist = $edit_dist, cluster size = $cluster_size "
         ./gsec-support/cliver.sh  -t "check-$edit_dist" -o "ch-$cluster_size" -c $client -i lsf -m $LSF_MEMORY_GB -x " -max-k-extension=$k_length -use-clustering-hint -cluster-size=$cluster_size " -b $DATA_TAG
