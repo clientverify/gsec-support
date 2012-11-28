7cdc03fac48940110025e51bb8c0ec0aeb74847b
Robby Cochran <rcochran@killdevil-login1.kd.unc.edu>
2012-11-27 22:09:20 -0500

    do-copy-results.sh: modified for new parameter sets

--- do-copy-results.sh
+++ do-copy-results.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
 
+SCRIPT=./gsec-support/verify_and_copy.sh
 BASE_DIR=$PWD/data
 DEST_HOST="rac@kudzoo.cs.unc.edu"
-DEST_DIR="/home/rac/research/test.gsec/results.oakall"
+DEST_DIR="/home/rac/research/test.gsec/results/fast.2"
+TRAINING_DIR="data/training"
 
-KPREFIX_LENGTHS="64 256 "
-CLUSTER_SIZES="8 16 32 256 512 1024"
+#KPREFIX_LENGTHS="8 16 64 128 256"
+#KPREFIX_LENGTHS="64"
+KPREFIX_LENGTHS="16"
+#CLUSTER_SIZES="4096"
+CLUSTER_SIZES="256"
+#MEDOID_COUNTS="4 8 16"
+#MEDOID_COUNTS="8 16"
+MEDOID_COUNTS="8"
+DATA_TAG="large-extra"
+
+#for k_length in $KPREFIX_LENGTHS; do
+#  for cluster_size in $CLUSTER_SIZES; do
+#    echo "K = $k_length, edit dist = $edit_dist, cluster size = $cluster_size "
+#    ./gsec-support/copy_results.sh -b large -d $DEST_HOST:$DEST_DIR -s $BASE_DIR/self-$k_length-$cluster_size
+#    ./gsec-support/copy_results.sh -b large -d $DEST_HOST:$DEST_DIR -s $BASE_DIR/ch-$k_length-$cluster_size
+#    ./gsec-support/copy_results.sh -b large -d $DEST_HOST:$DEST_DIR -s $BASE_DIR/nc-$k_length-$cluster_size
+#  done
+#done
+
+#echo "Copying results from self verification"
+eval $SCRIPT -b $DATA_TAG -d $DEST_HOST:$DEST_DIR -v $TRAINING_DIR -s $BASE_DIR/self
+eval $SCRIPT -b $DATA_TAG -d $DEST_HOST:$DEST_DIR -v $TRAINING_DIR -s $BASE_DIR/self-t
+
+#echo "Copying results from naive verification"
+#eval $SCRIPT -b $DATA_TAG -d $DEST_HOST:$DEST_DIR -v $TRAINING_DIR -s $BASE_DIR/naive
+#eval $SCRIPT -b $DATA_TAG -d $DEST_HOST:$DEST_DIR -v $TRAINING_DIR -s $BASE_DIR/naive-t
 
 for k_length in $KPREFIX_LENGTHS; do
   for cluster_size in $CLUSTER_SIZES; do
-    echo "K = $k_length, edit dist = $edit_dist, cluster size = $cluster_size "
-    ./gsec-support/copy_results.sh -b large -d $DEST_HOST:$DEST_DIR -s $BASE_DIR/self-$k_length-$cluster_size
-    ./gsec-support/copy_results.sh -b large -d $DEST_HOST:$DEST_DIR -s $BASE_DIR/ch-$k_length-$cluster_size
-    ./gsec-support/copy_results.sh -b large -d $DEST_HOST:$DEST_DIR -s $BASE_DIR/nc-$k_length-$cluster_size
+    eval $SCRIPT -b $DATA_TAG -d $DEST_HOST:$DEST_DIR -v $TRAINING_DIR -s $BASE_DIR/hint-$cluster_size-$k_length
+    eval $SCRIPT -b $DATA_TAG -d $DEST_HOST:$DEST_DIR -v $TRAINING_DIR -s $BASE_DIR/hint-$cluster_size-$k_length-t
+    for max_medoid_count in $MEDOID_COUNTS; do
+      eval $SCRIPT -b $DATA_TAG -d $DEST_HOST:$DEST_DIR -v $TRAINING_DIR -s $BASE_DIR/msg-$cluster_size-$k_length-$max_medoid_count
+      eval $SCRIPT -b $DATA_TAG -d $DEST_HOST:$DEST_DIR -v $TRAINING_DIR -s $BASE_DIR/msg+hint-$cluster_size-$k_length-$max_medoid_count-t
+    done
   done
 done
 
-
+ 
