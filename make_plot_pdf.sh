e42c5a9b6e9db6be2a2823e4071616702e58ec9a
Robby Cochran <rac@cs.unc.edu>
2015-03-05 16:04:43 -0500

    [make_plot_pdf.sh] add comments and clean up intermediate files that are generated

--- make_plot_pdf.sh
+++ make_plot_pdf.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
+###############################################################################
+### Use Latex to generate a PDF from a directory of figures images
+###############################################################################
+
+set -u # Exit if uninitialized value is used
+set -e # Exit on non-true value
+set -o pipefail # exit on fail of any command in a pipe
+
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ERROR_EXIT=1
+PROG=$(basename $0)
+
+# gsec_common required variables
+ROOT_DIR="`pwd`"
+VERBOSE_OUTPUT=0
+
+# Include gsec_common
+. $HERE/gsec_common
+
 FIGURE_DIR=$1
+if [ "$#" -ne 1 ]
+then
+  echo -e "Usage:\nGenerate PDF with Latex from directory of figures.\n\t$0: [directory of figures]"
+  exit 1
+fi
+
+FIGURE_TYPE="png"
+OUTPUT_DIR=$(dirname ${FIGURE_DIR})
 TEXBASE=$(basename ${FIGURE_DIR})
-TEXFILE="${TEXBASE}.tex"
+TEXFILE="${OUTPUT_DIR}/${TEXBASE}.tex"
+PDFFILE="${OUTPUT_DIR}/${TEXBASE}.pdf"
+
+FIGURE_COUNT=$(find ${FIGURE_DIR} -follow -maxdepth 1 -iname "*.${FIGURE_TYPE}" | wc -l)
+lecho "Generating $(basename ${PDFFILE}) from ${FIGURE_COUNT} figures"
 
 TEXHEADER="\documentclass[letterpaper,twocolumn,10pt]{article} \usepackage{graphicx} \usepackage{url} \renewcommand{\figurename}{} \begin{document}"
 FIGURE_TEMPLATE="
@@ -29,7 +60,10 @@ do
 done
 echo ${TEXFOOTER} >> ${TEXFILE}
 
-OUTPUT_DIR=$(dirname ${FIGURE_DIR})
+# Generate PDF from latex
 pdflatex -output-directory ${OUTPUT_DIR} ${TEXFILE} > /dev/null
+
+# Delete intermediate files
+rm ${TEXFILE}
 rm ${OUTPUT_DIR}/${TEXBASE}.aux
 rm ${OUTPUT_DIR}/${TEXBASE}.log
