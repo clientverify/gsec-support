cc1e1ec7d6f0f3bce14bcfb825747df42b7bfc7b
Robby Cochran <rac@cs.unc.edu>
2015-10-16 12:08:01 -0400

    also print out stat summary tables

--- make_graphs.r
+++ make_graphs.r
@@ -68,7 +68,7 @@ legend_rows = ceiling(length(unique(factor(data$mode)))/3)
 ###############################################################################
 
 # columns to skip
-skipcolnames = c('Bin','trace','RoundNumber','Queries')
+skipcolnames = c('Bin','trace','RoundNumber','Queries','Quer','SocketEvent')
 
 # plot data that is numeric and non-zero
 plotnames = c()
@@ -80,7 +80,8 @@ for (col in colnames(data)) {
     cat("Not numeric: ", col, "\n")
   }
   if (is.numeric(data[[col]]) && sum(as.numeric(data[[col]])) != 0) {
-    if (! is.element(col,skipcolnames)) {
+    matches <- unique(grep(paste(skipcolnames,collapse="|"),col,value=TRUE))
+    if (length(matches) == 0) {
       plotnames = c(plotnames, col)
     }
   }
@@ -184,30 +185,11 @@ if (tag == "ktest-timefix" | tag == "ktest-single-1") {
   #}
   #quit(status=0)
 } else {
-names(data)[names(data)=="RoundRealTime"] <- "Time"
-names(data)[names(data)=="VerifierDelayTime"] <- "Delay"
-names(data)[names(data)=="SocketEventSize"] <- "MessageSize"
-names(data)[names(data)=="SocketEventSizeBytes"] <- "MessageSizeBytes"
-
-
-if (length(selected_modes) != 0) {
- mode_params = selected_modes_alt_names
- y_params = c("Time","Delay")
- params = list()
- for (m in seq(length(mode_params))) {
-  for (y in seq(length(y_params))) {
-   params[[length(params)+1]] = c(mode_params[[m]], y_params[[y]])
-  }
- }
- plotwidth = default_plotwidth
- plotheight = default_plotheight/2
- results = mclapply(params, do_box_alt_log_plot, mc.cores=num_threads)
- results = mclapply(params, do_box_alt_plot, mc.cores=num_threads)
-}
-
 plotwidth = default_plotwidth
 plotheight = default_plotheight
-
+makePlots = TRUE
+makePlots = FALSE
+if(makePlots) {
 #results = mclapply(plotnames, do_box_plot, mc.cores=num_threads)
 #results = mclapply(plotnames, do_log_box_plot, mc.cores=num_threads)
 
@@ -234,5 +216,95 @@ plotheight = default_plotheight
 plotwidth = default_plotwidth*0.75
 do_time_summary_plot()
 do_instruction_summary_plot()
+}
+
+names(data)[names(data)=="RoundRealTime"] <- "Time"
+names(data)[names(data)=="VerifierDelayTime"] <- "Delay"
+names(data)[names(data)=="VerifierWaitTime"] <- "Wait"
+names(data)[names(data)=="BackTrackCount"] <- "Backtracks"
+names(data)[names(data)=="SocketEventSize"] <- "MessageSize"
+names(data)[names(data)=="SocketEventSizeBytes"] <- "MessageSizeBytes"
+
+
+if (length(selected_modes) != 0) {
+ mode_params = selected_modes_alt_names
+ #y_params = c("Time","Delay","InstructionCount")
+ y_params = c("Time","Delay","Wait","Backtracks")
+ params = list()
+ for (m in seq(length(mode_params))) {
+  for (y in seq(length(y_params))) {
+   params[[length(params)+1]] = c(mode_params[[m]], y_params[[y]])
+  }
+ }
+
+ plotwidth = default_plotwidth
+ plotheight = default_plotheight/2
+ if(makePlots) {
+ results = mclapply(params, do_box_alt_log_plot, mc.cores=num_threads)
+ results = mclapply(params, do_box_alt_plot, mc.cores=num_threads)
+ }
+
+ #stat_names=c("nbr.val","min","max","median","mean","var","std.dev")
+ #stat_names=c("min","max","median","mean","var","std.dev")
+ stat_names=c("nbr.val", "min","max","median","mean","var","std.dev","sum")
+
+ options(scipen=100)
+# options(digits=4)
+  allstats = data.frame()
+for (y in seq(length(y_params))) {
+  for (m in seq(length(mode_params))) {
+     #params[[length(params)+1]] = c(mode_params[[m]], y_params[[y]])
+     #mdata <- melt(subset(data, mode == m), id=c(m), measure=c(y))
+     #mdata <- melt(data, id=c("mode"), measure=c(y_params[[y]]))
+     #mdata <- melt(subset(data, mode == mode_params[[m]]), id=c("mode"), measure=c(y_params[[y]]))
+     sdata <- subset(data, mode == mode_params[[m]])
+     #mdata <- cbind(sdata$Time, sdata$Delay)
+     #mdata <- sdata[c("Time","Delay")]
+     mdata <- sdata[y_params[[y]]]
+     stats <- stat.desc(mdata)
+     cat("\nMode stat: ",mode_params[[m]]," ", y_params[[y]],"\n")
+     #cat("\nMode stat: ",mode_params[[m]],"\n")
+     print(stats)
+     #print(xtable(t(stats)))
+     tstats <- t(stats)
+     tstats <- tstats[,stat_names,drop=FALSE]
+     tstats <- data.frame(t(tstats))
+     #tstats <- tstats[stat_names,]
+     names(tstats)[names(tstats)==y_params[[y]]] <- paste(mode_params[[m]], y_params[[y]] ,sep="_")
+     tstats <- t(tstats)
+
+     #len = length(tstats[,1])
+     ##tstats$mode <- rep(mode_params[[m]], len)
+     ##Mode <- rep(mode_params[[m]], len)
+     #Mode <- rep(paste(y_params[[y]], mode_params[[m]],sep=" "), len)
+     ##tstats <- cbind(rep(paste(y_params[[y]], mode_params[[m]],sep=" "), len), tstats)
+     #tstats <- cbind(Mode, tstats)
+     allstats <- rbind(allstats, tstats)
+     print(tstats)
+
+     #cat("\nMax ", y_params[[y]]," Row:\n")
+     #print(subset(sdata, eval(parse(y_params[[y]] == stats["max", y_params[[y]]]))))
+
+     cat("\nMax Time Row:\n")
+     print(subset(sdata, Time == stats["max","Time"]))
+
+     cat("\nMax Delay Row:\n")
+     print(subset(sdata, Delay == stats["max","Delay"]))
+   }
+ }
+ stat_table <- xtable(t(allstats))
+ digits(stat_table) <- 4
+ print(stat_table)
+
+ #print(data$Backtracks)
+
+ data_bt = subset(data, Backtracks > 1)
+ data_nobt = subset(data, Backtracks <= 1)
+ print(stat.desc(data_bt$Time))
+ print(stat.desc(data_nobt$Time))
+
+
+}
+
 
 }
