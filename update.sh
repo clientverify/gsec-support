af8592c0962d1ad8b668ecc3050bd77d347110da
Andrew Chi <achi@cs.unc.edu>
2016-08-31 11:38:00 -0400

    [update.sh] use system boost libraries

--- update.sh
+++ update.sh
@@ -1601,7 +1601,8 @@ main()
 
     install_wllvm
     install_google_perftools
-    install_boost
+    # Boost is still required, but we can use the system version
+    #install_boost
     install_uclibc_git
     install_ncurses
     install_minisat
